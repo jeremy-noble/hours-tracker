8f0a37d3e00e83ce9614688e9377b15352c1b4e7
Jeremy Kay <jeremy@nobledesktop.com>
2012-08-31 13:07:57 -0400

    links for time_sheets

--- app/controllers/time_sheets_controller.rb
+++ app/controllers/time_sheets_controller.rb
@@ -20,7 +20,7 @@ class TimeSheetsController < ApplicationController
   # GET /time_sheets/1
   # GET /time_sheets/1.json
   def show
-    @time_sheet = TimeSheet.find(params[:id])
+    @time_sheet = @user.time_sheets.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -41,18 +41,18 @@ class TimeSheetsController < ApplicationController
 
   # GET /time_sheets/1/edit
   def edit
-    @time_sheet = TimeSheet.find(params[:id])
+    @time_sheet = @user.time_sheets.find(params[:id])
   end
 
   # POST /time_sheets
   # POST /time_sheets.json
   def create
-    @time_sheet = TimeSheet.new(params[:time_sheet])
+    @time_sheet = @user.time_sheets.new(params[:time_sheet])
 
     respond_to do |format|
       if @time_sheet.save
-        format.html { redirect_to @time_sheet, notice: 'Time sheet was successfully created.' }
-        format.json { render json: @time_sheet, status: :created, location: @time_sheet }
+        format.html { redirect_to [@user, @time_sheet], notice: 'Time sheet was successfully created.' }
+        format.json { render json: [@user, @time_sheet], status: :created, location: [@user, @time_sheet] }
       else
         format.html { render action: "new" }
         format.json { render json: @time_sheet.errors, status: :unprocessable_entity }
@@ -63,11 +63,11 @@ class TimeSheetsController < ApplicationController
   # PUT /time_sheets/1
   # PUT /time_sheets/1.json
   def update
-    @time_sheet = TimeSheet.find(params[:id])
+    @time_sheet = @user.time_sheets.find(params[:id])
 
     respond_to do |format|
       if @time_sheet.update_attributes(params[:time_sheet])
-        format.html { redirect_to @time_sheet, notice: 'Time sheet was successfully updated.' }
+        format.html { redirect_to [@user, @time_sheet], notice: 'Time sheet was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -79,11 +79,11 @@ class TimeSheetsController < ApplicationController
   # DELETE /time_sheets/1
   # DELETE /time_sheets/1.json
   def destroy
-    @time_sheet = TimeSheet.find(params[:id])
+    @time_sheet = @user.time_sheets.find(params[:id])
     @time_sheet.destroy
 
     respond_to do |format|
-      format.html { redirect_to time_sheets_url }
+      format.html { redirect_to user_time_sheets_url }
       format.json { head :no_content }
     end
   end
