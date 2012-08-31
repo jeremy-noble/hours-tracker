15736b7b72116f739f051312a5c905c30349a3c5
Jeremy Kay <jeremy@nobledesktop.com>
2012-08-31 12:15:47 -0400

    twitter bootstrap and users scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hours::Application.config.secret_token = '8be24db832625f6e8f6af5de070d9a7a03b7a75692452bfa8f29fb404e4bfb7fead5f20e36064ab90efd1ef9e23c01a70a1a71e1209113e11e9d0ca694411959'
