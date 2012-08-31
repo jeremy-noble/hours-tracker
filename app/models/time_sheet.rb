aeb74dc7ee81c9a33baaa8611af314f573f02901
Jeremy Kay <jeremy@nobledesktop.com>
2012-08-31 14:24:07 -0400

    got basics working

--- app/models/time_sheet.rb
+++ app/models/time_sheet.rb
@@ -2,4 +2,14 @@ class TimeSheet < ActiveRecord::Base
   attr_accessible :paid, :user_id
   belongs_to :user
   has_many :entries
+
+  def total_hours
+    total_hours = 0
+    if self.entries
+      self.entries.each do |entry|
+        total_hours = total_hours + entry.hours
+      end
+    return total_hours.to_s
+    end
+  end
 end
