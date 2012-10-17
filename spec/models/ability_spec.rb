df5ed9d3d4222401808ef2a499b4ee3d4f2ff503
Jeremy Kay <jeremykaybass@gmail.com>
2012-10-17 12:39:15 -0400

    update ability_spec to more explicitly use current_user

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 describe 'User' do
   describe 'abilities' do
     subject { ability }
-    let(:ability){ Ability.new(user) }
+    let(:ability){ Ability.new(current_user) }
 
     context 'when is not logged in' do
-      let(:user){ nil }
+      let(:current_user){ nil }
       # let(:user){ FactoryGirl.create(:user) }
 
       it{ should_not be_able_to(:manage, :all) }
@@ -13,12 +13,12 @@ describe 'User' do
     end
 
     context 'when is a logged in normal user' do
-      let(:user){ FactoryGirl.create(:user) } 
+      let(:current_user){ FactoryGirl.create(:user) } 
       let(:wrong_user){ FactoryGirl.create(:user, email: "wronguser@wrongywrong.com") }   
 
       it{ should_not be_able_to(:manage, :all) }
       it{ should_not be_able_to(:read, :all) }
-      it{ should be_able_to(:read, user) }
+      it{ should be_able_to(:read, current_user) }
       it{ should_not be_able_to(:read, wrong_user) }
     end
 
