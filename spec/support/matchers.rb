daa2861333e12cc70fb1f7f0198d370a4a1ab717
Jeremy Kay <jeremykaybass@gmail.com>
2012-11-12 11:16:44 -0500

    get rid of Rspec deprecation warnings

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -20,7 +20,7 @@ def has_exact_link?(locator, options={})
 end
 
 
-Rspec::Matchers.define :appear_before do |later_content|
+RSpec::Matchers.define :appear_before do |later_content|
   match do |earlier_content|
     page.body.index(earlier_content) < page.body.index(later_content)
   end
